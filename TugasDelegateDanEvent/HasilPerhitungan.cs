012b5b7a29bb352fc240f5ca820d4a8f4111f263
Wildan Amru <Wildan Amru@LAPTOP-WILDAN>
2022-12-05 21:38:09 +0700

    update minor

--- TugasDelegateDanEvent/HasilPerhitungan.cs
+++ TugasDelegateDanEvent/HasilPerhitungan.cs
@@ -16,10 +16,10 @@ namespace TugasDelegateDanEvent
         {
             InitializeComponent();
         }
-        private void proses(int a, int b, string operasi, string Label, float hasil)
+        private void proses(int a, int b, string operasi, string label, float hasil)
         {
             listHasil.Items.Add(
-                String.Format($"Hasil {Label} dari {a} {operasi} {b} = ") +
+                String.Format($"Hasil {label} dari {a} {operasi} {b} = ") +
                 String.Format(hasil % 1 == 0 ? "{0:0}" : "{0:0.00}", hasil) // Atur untuk 2 angka di belakang koma
             );
         }
